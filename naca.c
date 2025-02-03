67e91cee3cc83aceb5b9cce0370c5f69efe0d8d6
jaredcain <jared.j.cain@gmail.com>
2025-02-03 17:57:52 -0500

    Add files via upload

--- naca.c
+++ naca.c
@@ -1,25 +1,33 @@
+/* I believe these are the only header files i need to include for the simple flow to find lift and drag */
+
 #include "embed.h"
 #include "navier-stokes/centered.h"
 #include "view.h"
 
-#define chord   (1.00)
-#define uref    (1.00)
-#define tt      (0.12)
+#define chord       (1.00)
+#define uref        (1.00)
+#define thickness   (0.12)
 
 double Re = 6.e6;
 double aoa = 0. * M_PI / 180.0;
-int maxlevel = 10;
+int level = 12;
+
+/* I've seen this in many examples, unsure what it does beyond mu being viscosity */
 
 face vector muv[];
 
+/* This is how i build the airfoil shape, unsure if this is sufficient, as it was mentioned that the trailing edge should be treated differently */
+
 double naca0012(double x, double y) {
   if (x >= 0. && x <= 1.) {
-    return sq(y) -sq(5. * tt * (0.2969 * sqrt(x) - 0.1260 * x - 0.3516 * sq(x) + 0.2843 * cube(x) - 0.1036 * pow(x, 4.)));
-    } 
+    return sq(y) -sq(5. * thickness * (0.2969 * sqrt(x) - 0.1260 * x - 0.3516 * sq(x) + 0.2843 * cube(x) - 0.1036 * pow(x, 4.)));
+   } 
   else {
     return 1.0;
   }
 }
+
+/* This is how aoa is applied, works well from what i can tell */
 
 double naca(double x, double y)
 {
@@ -27,6 +35,8 @@ double naca(double x, double y)
   double yr = x * sin(aoa) + y * cos(aoa);
   return naca0012(xr, yr);
 }
+
+/* main function where i set up being able to choose AoA, have the logfile, set the domain size and number of cells, unsure again what mu = muv does, this is the theory side that I'm lacking on */
 
 int main(int argc, char *argv[])
 {
@@ -38,18 +48,22 @@ int main(int argc, char *argv[])
   snprintf(log_filename, sizeof(log_filename), "%.0f.log", aoa * 180.0 / M_PI);
   freopen(log_filename, "w", stderr);
   
-  L0 = 8.;
-  origin (-1, -L0/2.);
-  N = 2048;
+  L0 = 16.;
+  origin (-4, -L0/2.);
+  N = 1 << level;
   mu = muv;
   run(); 
 }
+
+/* I understand this is viscosity, unsure if it is the only think needed, and if it is done correctly */
 
 event properties (i++)
 {
   foreach_face()
     muv.x[] = (uref)*(chord)/(Re)*fm.x[];
 }
+
+/* BC's, I think some may  be redundant and/or unnecessary */
 
 u.n[left]  = dirichlet(1);
 p[left]    = neumann(0);
@@ -62,11 +76,15 @@ pf[right]  = dirichlet(0);
 u.n[embed] = dirichlet(0);
 u.t[embed] = dirichlet(0);
 
+/* Initialization, this embeds the airfoil shape? think u.x=1 may be redundant based on BCs */
+
 event init (t = 0) {
   solid(cs, fs, naca(x, y));
   foreach()
-    u.x[] =  1.;
+    u.x[] = 1.;
 }
+
+/* Fairly certain all is correct here, but I'm still having some issues with negative drag coefficients */
 
 event logfile (i += 10) {
   scalar omega[];
@@ -78,20 +96,20 @@ event logfile (i += 10) {
   fprintf(stderr, "%.4d\t%.4e\t%.4e\t%.4e\n", i, t, CD, CL);
 }
 
-event movies (t += 0.05, t <= 120)
+event movies (t += 0.05, t <= 30)
 {
-  scalar p[];
-  view(quat = {0.000, 0.000, 0.000, 1.000}, fov = 30, near = 0.01, far = 1000, tx = -0.10, ty = 0.00, tz = -0.25, width = 1024, height = 512);
+  scalar p[], m[];
+  view(quat = {0.000, 0.000, 0.000, 1.000}, fov = 30, near = 0.01, far = 1000, tx = -0.10, ty = 0.00, tz = -0.35, width = 1024, height = 512);
   box();
-  squares(color = "p"); 
-  cells();
+  squares(color = "p", linear=true); 
   draw_vof(c = "cs", lw = 1, lc = {0, 0, 0});
-
   char video[50];
   snprintf(video, sizeof(video), "%.0f.mp4", aoa * 180.0 / M_PI);
   save(video);
 }
 
+/* Adaptive Mesh, not sure where minlevel should be, if tolerances are low enough and/or too low */
+
 event adapt (i++) {
-  adapt_wavelet ({cs,u}, (double[]){1e-3,1e-3,1e-3}, maxlevel, 3);
+  adapt_wavelet ({cs,u.x,u.y}, (double[]){1e-2,5e-3,5e-3}, maxlevel=level, minlevel=4);
 }
